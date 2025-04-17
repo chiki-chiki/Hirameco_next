4a3d36d93a975fb45249bdb592f408cf968b77ad
chiki-chiki <tsubame412@icloud.com>
2025-04-17 22:21:44 +0900

    ささやき複数表示

--- app/components/Whisper.tsx
+++ app/components/Whisper.tsx
@@ -3,15 +3,15 @@
 import {useState} from'react'
 
 export default function Whisper(){
-    const [title,setTitle]=useState<string|null>(null)
+    const [titles,setTitles]=useState<string[]>([])
 
-    const fetchRandomArticle=async()=>{
+    const fetchRandomArticles=async()=>{
         const res=await fetch(
-            'https://ja.wikipedia.org/w/api.php?action=query&list=random&rnlimit=1&format=json&origin=*'
+            'https://ja.wikipedia.org/w/api.php?action=query&list=random&rnlimit=3&format=json&origin=*'
         )
         const data =await res.json()
-        const randomTitle=data.query.random[0].title
-        setTitle(randomTitle)
+        const randomTitles=data.query.random.map((item:{title:string})=>item.title)
+        setTitles(randomTitles)
 
     }
 
@@ -19,17 +19,24 @@ export default function Whisper(){
         <div className="p-4 bg-gray-50 rounded shadow mt-6 text-center">
             <h2 className="text-lg font-bold mb-4">ささやき(wikipedia)</h2>
 
-            {title?(
-                <a href={`https://ja.wikipedia.org/wiki/${encodeURIComponent(title)}`}
-                target="_blank"
-                rel="noopener noreferrer"
-                className='text-blue-600 hover:underline block mb-4'>
-                    {title}
-                </a>
+            {titles.length>0?(
+                <ul className='space-y-2 mb-4'>
+                    {titles.map((title,idx)=>(
+                        <li key={idx}>
+                            <a href={`https://ja.wikipedia.org/wiki/${encodeURIComponent(title)}`}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className='text-blue-600 hover:underline '>
+                                {title}
+                            </a>
+
+                        </li>
+                    ))}
+                </ul>
             ):(
               <p className='text-gray-500 mb-4'>まだささやきがないよ </p>  
             )}
-            <button onClick={fetchRandomArticle}
+            <button onClick={fetchRandomArticles}
             className='px-4 py-2 bg-indigo-500 text-white rounded hover:bg-indigo-600 transition'>
                 ささやきを聴く
             </button>
